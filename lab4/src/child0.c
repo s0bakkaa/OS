26b8c05e5247b536978e3b8eda68682b5052e14d
Yulia Shatunova <90799835+s0bakkaa@users.noreply.github.com>
2021-11-15 13:39:37 +0300

    Update child0.c

--- lab4/src/child0.c
+++ lab4/src/child0.c
@@ -12,22 +12,31 @@
 #include "memwork.h"
 
 int main(int argc, char **argv) {
-	int map_fd = shm_open(BACKING_FILE, O_RDWR, FILE_MODE);
+	// открываем родительское
+	int fd = shm_open(BACKING_FILE, O_RDWR, FILE_MODE);
+    if (fd < 0) { //  в случае успеха: возврат неотрицательного дескриптора
+    	perror("SHM_OPEN_child0_1");
+    	exit(EXIT_FAILURE);
+    }
+
+	// открываем для потомка 0
+	int map_fd = shm_open(BACKING_FILE_0, O_RDWR | O_CREAT, FILE_MODE);
 	if (map_fd < 0) {
-		perror("SHM_OPEN");
+		perror("SHM_OPEN_child0_2");
 		exit(EXIT_FAILURE);
 	}
 	
 	struct stat statbuf;
-	fstat(map_fd, &statbuf);
+	//fstat(map_fd, &statbuf);
+	fstat(fd, &statbuf);
 	const size_t map_size = statbuf.st_size;
-	caddr_t mem_ptr = mmap(NULL, map_size, PROT_READ | PROT_WRITE, MAP_SHARED, map_fd, 0);
+	caddr_t mem_ptr = mmap(NULL, map_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 	if (mem_ptr == MAP_FAILED) {
 		perror("MMAP");
 		exit(EXIT_FAILURE);		
 	}
 	
-	sem_t *sem_ptr = sem_open(SEMAPHORE_NAME, O_CREAT, FILE_MODE, 2);
+	sem_t *sem_ptr = sem_open(SEMAPHORE, FILE_MODE, 2);
 	if (sem_ptr == SEM_FAILED) {
 		perror("SEM_OPEN");
 		exit(EXIT_FAILURE);
@@ -39,16 +48,37 @@ int main(int argc, char **argv) {
 	
 	char *str_in = (char *) malloc(map_size * sizeof(char));
 	for (int index = 0; index < map_size; ++index) {
-		str_in[index] = tolower(mem_ptr[index]);
+		str_in[index] = mem_ptr[index];
 	}
-	memset(mem_ptr, '\0', map_size);
-	sprintf(mem_ptr, "%s", str_in);
+	
+	if (ftruncate(map_fd, (off_t)map_size) == -1) {
+    	perror("FTRUNCATE_child0");
+    	exit(EXIT_FAILURE);
+    }
+	
+	caddr_t mem_ptr_0 = mmap(NULL, map_size, PROT_READ | PROT_WRITE, MAP_SHARED, map_fd, 0);
+	if (mem_ptr == MAP_FAILED) {
+		perror("MMAP");
+		exit(EXIT_FAILURE);		
+	}
+
+	char *str_out = (char *) malloc(map_size * sizeof(char));
+	for (int index = 0; index < map_size; ++index) {
+		str_out[index] = tolower(str_in[index]);
+	}
+	
+	memset(mem_ptr_0, '\0', map_size);
+	sprintf(mem_ptr_0, "%s", str_out);
 	free(str_in);
+	free(str_out);
 	pid_t pid = fork();
 	if (pid == 0) {
 		munmap(mem_ptr, map_size);
-		close(map_fd);
-		sem_close(sem_ptr);
+		close(fd);
+		//close(map_fd);
+		shm_unlink(BACKING_FILE);
+		//shm_unlink(BACKING_FILE_0);
+		//sem_close(sem_ptr);
 		execl("child1", "child1", NULL);
 		perror("EXECL");
 		exit(EXIT_FAILURE);
