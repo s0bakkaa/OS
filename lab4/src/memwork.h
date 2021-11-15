f7165ba1b0f295b553ad0ac2a07ed916d509ac91
Yulia Shatunova <90799835+s0bakkaa@users.noreply.github.com>
2021-11-15 13:38:54 +0300

    Update memwork.h

--- lab4/src/memwork.h
+++ lab4/src/memwork.h
@@ -4,7 +4,11 @@
 #include <fcntl.h>
 
 const char *BACKING_FILE = "lab4.back";
-const char *SEMAPHORE_NAME = "lab4.semaphore";
+const char *BACKING_FILE_0 = "lab4_0.back";
+const char *BACKING_FILE_1 = "lab4_1.back";
+const char *SEMAPHORE = "lab4.semaphore";
+const char *SEMAPHORE_0 = "lab4_0.semaphore";
+const char *SEMAPHORE_1 = "lab4_1.semaphore";
 unsigned FILE_MODE = S_IWUSR | S_IRUSR | S_IRGRP | S_IROTH; //  всегда должен быть указан для O_CREAT
 
 #endif //SRC__MEMWORK_H_
